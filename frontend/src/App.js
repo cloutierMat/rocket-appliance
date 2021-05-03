31cc1afb828eb4f971d317b67e2a344d1d155d6e
azdelta <azdelta@users.noreply.github.com>
2021-05-03 15:17:16 -0600

    Main page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
+import Main from './pages/mainPage/Main'
 function App() {
+  function thePageToDisplay() {
+    return <Main/>
+  }
   return (
     <div className="App">
       <div><nav>navBar</nav></div>
+      {thePageToDisplay()}
     </div>
   );
 }
