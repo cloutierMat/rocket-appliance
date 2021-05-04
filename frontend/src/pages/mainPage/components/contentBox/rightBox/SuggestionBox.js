137f1b962380e39863b7647749597926e56ded06
Mathieu Cloutier <cloutier.mat0@gmail.com>
2021-05-04 09:52:07 -0600

    fixed typo and added .vscode settings

--- frontend/src/pages/mainPage/components/contentBox/rightBox/SuggestionBox.js
+++ frontend/src/pages/mainPage/components/contentBox/rightBox/SuggestionBox.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import SuggestionGameLogo from "./SuggestionGameLogo";
 import SuggestionGameDescription from "./SuggestionGameDescription";
-export default function () {
+export default function SuggestionsBox() {
 	return (
 		<div>
 			<SuggestionGameLogo />
