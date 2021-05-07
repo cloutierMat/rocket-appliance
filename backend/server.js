a8fffb178c2d3a682eada53ed28bf39c937682db
Will Smit <wsmiteh@gmail.com>
2021-05-07 14:51:52 -0600

    backend creation
    server.js
        -server is connected (05/07)

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const logger = require("morgan");
+const app = express();
+const port = process.env.PORT || 8080;
+
+// express environment setup
+app.use(express.json());
+app.use(logger("dev"));
+
+// routes
+const gameRouter = require('./routes/game');
+app.use('/game', gameRouter);
+
+app.listen(port, () => {
+    console.log('server listening on port ' + port)
+    });
+
