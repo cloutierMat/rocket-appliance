9593bd453d09818c557b8bade2ee66c882c1af87
Mathieu Cloutier <cloutier.mat0@gmail.com>
2021-05-11 23:30:18 -0600

    JRA-1 #comment test on connecting to Jira

--- frontend/src/pages/mainPage/components/contentBox/leftBox/GameCard.js
+++ frontend/src/pages/mainPage/components/contentBox/leftBox/GameCard.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
 export default function GameCard(props) {
-   const { name, onMouseEnter, pagePointer } = props;
-   return (
-      <div
-         className="game-card card-rotate"
-         onMouseEnter={onMouseEnter}
-         onClick={() => {
-            pagePointer(name);
-         }}
-      >
-         {name}
-      </div>
-   );
+	const { name, onMouseEnter, pagePointer } = props;
+	return (
+		<div
+			className="game-card card-rotate"
+			onMouseEnter={onMouseEnter}
+			onClick={() => {
+				pagePointer(name);
+			}}
+		>
+			{name}
+		</div>
+	);
 }
