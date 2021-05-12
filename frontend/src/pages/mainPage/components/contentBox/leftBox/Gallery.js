41b20613f979404a14108fb217d7f1fe7c03013e
Mathieu Cloutier <cloutier.mat0@gmail.com>
2021-05-11 23:31:54 -0600

    JRA-2 #comment test on connecting to Jira

--- frontend/src/pages/mainPage/components/contentBox/leftBox/Gallery.js
+++ frontend/src/pages/mainPage/components/contentBox/leftBox/Gallery.js
@@ -2,21 +2,21 @@ import React from 'react';
 import GameCard from './GameCard';
 
 export default function Gallery(props) {
-   const { onMouseEnter, pagePointer } = props;
-   return (
-      <div className="gallery-wrapper">
-         <div className="gallery">
-            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(name => {
-               return (
-                  <GameCard
-                     key={name}
-                     name={name}
-                     onMouseEnter={onMouseEnter}
-                     pagePointer={pagePointer}
-                  />
-               );
-            })}
-         </div>
-      </div>
-   );
+	const { onMouseEnter, pagePointer } = props;
+	return (
+		<div className="gallery-wrapper">
+			<div className="gallery">
+				{[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(name => {
+					return (
+						<GameCard
+							key={name}
+							name={name}
+							onMouseEnter={onMouseEnter}
+							pagePointer={pagePointer}
+						/>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
