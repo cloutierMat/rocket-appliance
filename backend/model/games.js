6928a3be8c6a1d01389e38344edb916e53cc37d5
Mathieu Cloutier <cloutier.mat0@gmail.com>
2021-05-17 14:31:20 -0600

    RA-9 #close #comment Defined Trivia Schema

--- backend/model/games.js
+++ backend/model/games.js
@@ -1,24 +1,32 @@
-require ('./db');
+require('./db');
 const mongoose = require('mongoose');
 
-const triviaSchema = new mongoose.Schema({ 
-    name: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    category: {
-        type: String,
-        required: true,
-    },
-    question: {
-        type: String,
-        required: true,
-    },
-    options: {
-        type: Array,
-        required: true,
-    },
+const triviaSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	category: {
+		type: String,
+		required: true,
+	},
+	author: {
+		type: String,
+		default: "anonymous"
+	},
+	questions: [
+		{
+			question: {
+				type: String,
+				required: true,
+			},
+			options: {
+				type: Array,
+				required: true,
+			},
+		}
+	]
 });
 
-module.exports = {Trivia:mongoose.model("Trivia", triviaSchema)};
+module.exports = { Trivia: mongoose.model("Trivia", triviaSchema) };
