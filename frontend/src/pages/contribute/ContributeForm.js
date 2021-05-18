cc273a5640ac6e4504caf6de0dc319366b3b6916
azdelta <azdelta@users.noreply.github.com>
2021-05-18 11:02:33 -0600

    form in progress

--- frontend/src/pages/contribute/ContributeForm.js
+++ frontend/src/pages/contribute/ContributeForm.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function ContributeForm() {
-  return(
+  const [contributeForm, setContributeForm] = useState();
+
+  function handleContributorSubmit(event) {
+    event.preventDefault();
+  }
+  function handleGameName() {
+
+  }
+
+  return (
     <div>
-      <form>
-        <label for="gameName">Game: </label>
-        <input type="text" id="gameName" name="gameName" ></input>
+      <form onSubmit={handleContributorSubmit}>
+
+        <label for="category">Which category your game is most related to </label>
+        <select name="category" id="category">
+          <option value="science">Science</option>
+          <option value="language">Language</option>
+          <option value="history">History</option>
+          <option value="trivia">Trivia</option>
+        </select><br />
+
+        <label for="gameName">Game </label>
+        <input type="text" id="gameName" name="gameName" onChange={handleGameName}></input><br />
+
+        <label for="contributor">Contributor / Author </label>
+        <input type="text" id="contributor" name="contributor" ></input><br />
+
+        <button type="submit">Create Game</button>
       </form>
     </div>
-  )
+  );
 }
