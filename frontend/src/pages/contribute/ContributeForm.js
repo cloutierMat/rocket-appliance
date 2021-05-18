30fb1a1701e0b117c7bfbc09f5c7470b409b5d09
Will Smit <wsmiteh@gmail.com>
2021-05-18 12:43:36 -0600

    extremely broken form

--- frontend/src/pages/contribute/ContributeForm.js
+++ frontend/src/pages/contribute/ContributeForm.js
@@ -1,35 +1,42 @@
 import React, { useState } from 'react';
+import TriviaForm from './contributeForms/TriviaForm'
+import FormFilter from './contributeForms/FormFilter'
 
 export default function ContributeForm() {
-  const [contributeForm, setContributeForm] = useState();
+  const [contributeForm, setContributeForm] = useState("Trivia");
+  const 
+
 
   function handleContributorSubmit(event) {
     event.preventDefault();
-  }
-  function handleGameName() {
+  };
 
-  }
+  let formToDisplay = <TriviaForm/>
+
+  function handleGameType(event) {
+    setContributeForm(event.target.value)
+    console.log(event.target.value)
+    // if (event.target.value === "trivia") {
+    //   formToDisplay = <TriviaForm/>
+    // }
+    // else
+    //   console.log("no")
+    event.target.value === "trivia" ? formToDisplay = <TriviaForm/> : console.log("nothing to display")
+  };
 
   return (
     <div>
-      <form onSubmit={handleContributorSubmit}>
-
-        <label for="category">Which category your game is most related to </label>
-        <select name="category" id="category">
-          <option value="science">Science</option>
-          <option value="language">Language</option>
-          <option value="history">History</option>
+      {/* <form onSubmit={handleContributorSubmit}>
+        <label for="gameType">What type of game do you want to make? </label>
+        <select name="gameType" id="gameType" onChange= { handleGameType }>
           <option value="trivia">Trivia</option>
-        </select><br />
-
-        <label for="gameName">Game </label>
-        <input type="text" id="gameName" name="gameName" onChange={handleGameName}></input><br />
-
-        <label for="contributor">Contributor / Author </label>
-        <input type="text" id="contributor" name="contributor" ></input><br />
-
-        <button type="submit">Create Game</button>
-      </form>
+          <option value="hangman">Hangman</option>
+          <option value="hangwoman">Hangwoman</option>
+          <option value="placeholder">Placeholder</option>
+        </select> <br/>
+      </form> */}
+    <FormFilter value= { selectedForm } onFormSelect= { handleGameType } />
     </div>
-  );
-}
+  )
+
+};
