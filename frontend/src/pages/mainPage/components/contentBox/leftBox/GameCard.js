01b15c9157098b9eba7b4c09fbcaf0ad10b90ee0
Mathieu Cloutier <cloutier.mat0@gmail.com>
2021-05-20 22:11:21 -0600

    RA-19 added ReactFitty to help with text sizing in the GameCard

--- frontend/src/pages/mainPage/components/contentBox/leftBox/GameCard.js
+++ frontend/src/pages/mainPage/components/contentBox/leftBox/GameCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BsQuestionSquare } from "react-icons/bs";
 import { Animated } from "react-animated-css";  //niki
+import { ReactFitty } from "react-fitty";
 
 export default function GameCard(props) {
 	const { game, onMouseEnter, setPagePointer } = props;
@@ -9,16 +10,16 @@ export default function GameCard(props) {
 		<Animated animationIn="bounceInLeft" animationOut="fadeOut" isVisible={true}>
 			<div
 				className="game-card card-rotate btn btn-background-slide" //niki
-				onMouseEnter={onMouseEnter ? () => onMouseEnter(name): undefined}
+				onMouseEnter={onMouseEnter ? () => onMouseEnter(name) : undefined}
 				onClick={() => {
 					setPagePointer(name);
 				}}
 			>
 				<div>
 					<button className="btn btn-background-slide">
-						<h4>{name}</h4>
+						<h4><ReactFitty wrapText={true}>{name}</ReactFitty></h4>
 						<hr></hr>
-						<h2>{category}</h2>
+						<h2><ReactFitty wrapText={true}>{category}</ReactFitty></h2>
 						<BsQuestionSquare className="icon-button" />
 					</button>
 				</div>
