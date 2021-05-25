13266249194f9c88aa56f42f61b7f72e542abdbe
Mathieu Cloutier <cloutier.mat0@gmail.com>
2021-05-24 21:36:20 -0600

    RA-27 #comment refactored Trivia and TriviaForm put the focus on added input fields

--- frontend/src/pages/contribute/components/Trivia.js
+++ frontend/src/pages/contribute/components/Trivia.js
@@ -1,83 +1,40 @@
 import React, { useState } from "react";
-import { useForm, Controller } from "react-hook-form";
-import { v1 as id } from "uuid";
 import TriviaForm from './forms/TriviaForm';
 
 export default function Trivia() {
-  let INITIAL_DATA = {
-    name: "",
-    category: "",
-    type: "Trivia",
-    author: "",
-    description: "",
-    questions: []
-  };
-  const [data, setData] = useState(INITIAL_DATA);
-  const [messageOnCreate, setMessageOnCreate] = useState("");
-  const { watch } = useForm();
-  const at = watch("at", 2);
+	let initialData = {
+		name: "",
+		category: "",
+		type: "Trivia",
+		author: "",
+		description: "",
+		questions: []
+	};
+	const [messageOnCreate, setMessageOnCreate] = useState("");
 
-  const handleInsertQuestion = () => {
-    let prevData = { ...data };
-    prevData.questions = [...data.questions, { id: id(), options: [], link: "" }];
-    setData(prevData);
-  };
+	function onSubmit(dataToPost) {
+		fetch('/game/trivia', {
+			method: "POST",
+			body: JSON.stringify(dataToPost),
+			headers: {
+				"Content-Type": "application/json"
+			},
+		})
+			.then(res => res.json())
+			.then(dataToPost => {
+				console.log(`New Game ${dataToPost.name} Created`);
+			})
+			.then(setMessageOnCreate(`New Game ${dataToPost.name} Created`))
+			.catch((error) => {
+				console.error("error", error);
+				setMessageOnCreate(`Fail to create the game! Try again!`);
+			});
+	}
 
-
-  const handleInsertOption = (questionIndex) => {
-    let prevData = { ...data };
-    prevData.questions[questionIndex].options = [...data.questions[questionIndex].options, { id: id() }];
-    setData(prevData);
-  };
-
-  const handleLinkChange = (event, index) => {
-    let prevData = { ...data };
-    prevData.questions[index].link = event.target.value;
-    setData(prevData);
-  };
-
-  const handleInfoChange = (event, element) => {
-    let prevData = { ...data };
-    prevData[element] = event.target.value;
-    setData(prevData);
-  };
-
-
-  const handleRemoveQuestion = index => {
-    let prevData = { ...data };
-    prevData.questions = [...data.questions.slice(0, index), ...data.questions.slice(index + 1)];
-    setData(prevData);
-  };
-
-  const handleRemoveOption = (optionIndex, questionIndex) => {
-    let prevData = { ...data };
-    prevData.questions[questionIndex].options = [...data.questions[questionIndex].options.slice(0, optionIndex), ...data.questions[questionIndex].options.slice(optionIndex + 1)];
-    setData(prevData);
-  };
-
-  function onSubmit(dataToPost) {
-    fetch('/game/trivia', {
-      method: "POST",
-      body: JSON.stringify(dataToPost),
-      headers: {
-        "Content-Type": "application/json"
-      },
-    })
-      .then(res => res.json())
-      .then(dataToPost => {
-        console.log(`New Game ${dataToPost.name} Created`);
-      })
-      .then(setMessageOnCreate(`New Game ${dataToPost.name} Created`))
-      .catch((error) => {
-        console.error("error", error);
-        setMessageOnCreate(`Fail to create the game! Try again!`);
-      });
-  }
-
-  return (
-    <>
-      <TriviaForm onSubmit={onSubmit} onChangeInfo={handleInfoChange} onRemoveQuestion={handleRemoveQuestion} onChangeLink={handleLinkChange} onRemoveOption={handleRemoveOption} onInsertOption={handleInsertOption} onInsertQuestion={handleInsertQuestion} data={data} />
-      <h2 className="message-on-create_contribute">{messageOnCreate}</h2>
-    </>
-  );
+	return (
+		<>
+			<TriviaForm onSubmit={onSubmit} initialData={initialData} />
+			<h2 className="message-on-create_contribute">{messageOnCreate}</h2>
+		</>
+	);
 }
--- frontend/src/pages/contribute/components/forms/TriviaForm.js
+++ frontend/src/pages/contribute/components/forms/TriviaForm.js
@@ -1,19 +1,78 @@
 import React, { useRef, useEffect, useState } from "react";
+import { v1 as id } from "uuid";
 import { useForm, Controller } from "react-hook-form";
 import Joi from "joi";
 
 export default function TriviaForm(props) {
-	const { onSubmit, onChangeInfo, onRemoveQuestion, onChangeLink, onRemoveOption, onInsertOption, onInsertQuestion, data } = props;
+	const { initialData, onSubmit } = props;
 
 	const [errorMessages, setErrorMessages] = useState([]);
+	const [data, setData] = useState(initialData);
+	const [optionsClicked, setOptionsClicked] = useState(true);
 
 	const { handleSubmit, control } = useForm();
 
 	const inputFocus = useRef(null);
+	const questionFocus = useRef();
+	const optionFocus = useRef();
 
+
+	const handleInsertQuestion = () => {
+		let prevData = { ...data };
+		prevData.questions = [...data.questions, { id: id(), options: [], link: "" }];
+		setData(prevData);
+	};
+
+
+	const handleInsertOption = (questionIndex) => {
+		let prevData = { ...data };
+		prevData.questions[questionIndex].options = [...data.questions[questionIndex].options, { id: id() }];
+		setData(prevData);
+		setOptionsClicked(optionsClicked => optionsClicked ? false : true);
+	};
+
+	const handleLinkChange = (event, index) => {
+		let prevData = { ...data };
+		prevData.questions[index].link = event.target.value;
+		setData(prevData);
+	};
+
+	const handleInfoChange = (event, element) => {
+		let prevData = { ...data };
+		prevData[element] = event.target.value;
+		setData(prevData);
+	};
+
+
+	const handleRemoveQuestion = index => {
+		let prevData = { ...data };
+		prevData.questions = [...data.questions.slice(0, index), ...data.questions.slice(index + 1)];
+		setData(prevData);
+	};
+
+	const handleRemoveOption = (optionIndex, questionIndex) => {
+		let prevData = { ...data };
+		prevData.questions[questionIndex].options = [...data.questions[questionIndex].options.slice(0, optionIndex), ...data.questions[questionIndex].options.slice(optionIndex + 1)];
+		setData(prevData);
+	};
+
+
+	//
+	// In the following functions we set the cursor focus
 	useEffect(() => {
 		inputFocus.current.focus();
 	}, []);
+
+	useEffect(() => {
+		if (data.questions.length) {
+			questionFocus.current.focus();
+		}
+	}, [data.questions]);
+
+	useEffect(() => {
+		if (data.questions.length)
+			optionFocus.current.focus();
+	}, [optionsClicked]);
 
 	function formValidator(idAsData) {
 		const questionsToPost = data.questions.map(question => {
@@ -54,6 +113,7 @@ export default function TriviaForm(props) {
 		if (error) {
 			setErrorMessages(error.message);
 		} else {
+			setErrorMessages(null);
 			onSubmit(dataToPost);
 		}
 	}
@@ -67,65 +127,80 @@ export default function TriviaForm(props) {
 				<h1>Trivia </h1>
 				<ul>
 					<li>
-						Category: <input type="text" placeholder="Rocket Science" onChange={(event) => { onChangeInfo(event, "category"); }} ref={inputFocus} />
+						Category: <input
+							type="text"
+							placeholder="Rocket Science"
+							onChange={(event) => { handleInfoChange(event, "category"); }}
+							ref={inputFocus} />
 					</li>
 
 					<li>
-						Game Name: <input type="text" placeholder="Rocket Trivia" onChange={(event) => { onChangeInfo(event, "name"); }} />
+						Game Name: <input
+							type="text"
+							placeholder="Rocket Trivia"
+							onChange={(event) => { handleInfoChange(event, "name"); }} />
 					</li>
 
 					<li>
-						Contributor: <input type="text" placeholder="Your name / You can leave it anonymous" onChange={(event) => { onChangeInfo(event, "author"); }} />
+						Contributor: <input
+							type="text"
+							placeholder="Your name / You can leave it anonymous"
+							onChange={(event) => { handleInfoChange(event, "author"); }} />
 					</li>
 
 					<li>
 						<label className="game-description_contributor">Description:</label>
-						<textarea cols="50" rows="5" placeholder="Tell me more about the game" onChange={(event) => { onChangeInfo(event, "description"); }} />
+						<textarea
+							cols="50"
+							rows="5"
+							placeholder="Tell us more about the game"
+							onChange={(event) => { handleInfoChange(event, "description"); }} />
 					</li>
-
 					{data.questions.map((question, questionIndex) => (
-						<li key={question.id}>
+						<li key={question.id} >
 							Question {questionIndex + 1}
 							<Controller
-								as={<input />}
-								name={question.id}
+								render={() => <input ref={questionFocus} />}
+								name="question.id"
 								control={control}
 								defaultValue=""
 							/>
 
-							<button onClick={() => { onRemoveQuestion(questionIndex); }}>Delete</button>
+							<button onClick={() => { handleRemoveQuestion(questionIndex); }}>Delete</button>
 							<ul>
 
 								<li>
-									Link: <input type="url" placeholder="Link to learn more" onChange={(event) => { onChangeLink(event, questionIndex); }} />
+									Link: <input
+										type="url"
+										placeholder="Link to learn more"
+										onChange={(event) => { handleLinkChange(event, questionIndex); }} />
 								</li>
 								{data.questions[questionIndex].options.map((option, optionIndex) => (
 									<li key={option.id}>
 										Option {optionIndex + 1}
 										<Controller
-											as={<input />}
+											render={() => <input ref={optionFocus} />}
 											name={option.id}
 											control={control}
 											defaultValue=""
 										/>
 
-										<button onClick={() => { onRemoveOption(optionIndex, questionIndex); }}>Delete</button>
+										<button onClick={() => { handleRemoveOption(optionIndex, questionIndex); }}>Delete</button>
 
 									</li>
 								))}
 							</ul>
 
 							<section>
-								<button type="button" onClick={() => onInsertOption(questionIndex)}>
-									New option
-       					</button>
+								<button type="button" onClick={() => handleInsertOption(questionIndex)}>New option</button>
 							</section>
 
 						</li>
-					))}
+					))
+					}
 				</ul>
 				<section>
-					<button type="button" onClick={onInsertQuestion}>
+					<button type="button" onClick={handleInsertQuestion}>
 						New question
         </button>
 				</section>
