215119b4cd28b4c465517b9dd0f1e9bc6d76aeba
azdelta <azdelta@users.noreply.github.com>
2021-05-26 21:00:58 -0600

    RA-32 RA-41 #comment React-Router implemented and partially established and logo is linked to main page

--- frontend/src/pages/trivia/Trivia.js
+++ frontend/src/pages/trivia/Trivia.js
@@ -1,32 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import GameContext from '../../context/GameContext';
 import Questions from './Questions';
 import TriviaEnding from './TriviaEnding';
 import './trivia.css';
 
-export default function Trivia(props) {
-	const {gameName} = props;
+export default function Trivia() {
 	const [category, setCategory] = useState();
 	const [name, setName] = useState();
 	const [questions, setQuestions] = useState();
 	const [author, setAuthor] = useState();
 	const [isEndReached, setIsEndReached] = useState();
 
+	const gameCtx = useContext(GameContext);
+
+	const { gameName } = useParams();
+
 	useEffect(() => {
-		async function fetchData() {
-			try {
-				const response = await fetch(`game/play/trivia/${gameName}`);
-				const triviaToDisplay = await response.json();
-				setCategory(triviaToDisplay.category);
-				setName(triviaToDisplay.name);
-				setAuthor(triviaToDisplay.author);
-				setQuestions(triviaToDisplay.questions);
-				setIsEndReached(false);
-			} catch (error) {
-				console.error("error", error);
-			}
-		}
-		fetchData();
-	}, []);
+		if (!gameCtx.list) return;
+		console.log("gameCtx.list", gameCtx.list);
+		const triviaToDisplay = gameCtx.list.find(elem => elem.name === gameName);
+		console.log(triviaToDisplay);
+		setCategory(triviaToDisplay.category);
+		setName(triviaToDisplay.name);
+		setAuthor(triviaToDisplay.author);
+		setQuestions(triviaToDisplay.questions);
+		setIsEndReached(false);
+	}, [gameCtx]);
 
 	return (
 		<div className="trivia-page">
