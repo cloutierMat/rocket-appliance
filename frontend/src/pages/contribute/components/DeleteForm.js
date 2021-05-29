eab9a594679312b83660a3e566b6286aeae9ef4f
azdelta <azdelta@users.noreply.github.com>
2021-05-29 13:36:50 -0600

    RA-30 Delete table is updating in real time

--- frontend/src/pages/contribute/components/DeleteForm.js
+++ frontend/src/pages/contribute/components/DeleteForm.js
@@ -2,14 +2,37 @@ import React, { useState, useContext, useEffect } from 'react';
 import GameContext from '../../../context/GameContext';
 
 export default function DeleteForm(props) {
-  const [gameList, setGameList] = useState();
+  const gameCtx = useContext(GameContext);
+  const [gameList, setGameList] = useState(gameCtx.list);
   const [messageOnDelete, setMessageOnDelete] = useState(null);
 
-  const gameCtx = useContext(GameContext);
+  const allGames =
+    <table>
+      <thead>
+        <tr>
+          <th>Check to delete</th>
+          <th>Game name</th>
+          <th>Category</th>
+          <th>Game type</th>
+        </tr>
+      </thead>
+      <tbody>
+        {gameList && gameList.map(game => {
+          return (<tr key={game.name}>
+            <td>
+              <button onClick={() => onDelete(game.name)}>X</button>
+            </td>
+            <td>{game.name} </td>
+            <td>{game.category}</td>
+            <td>{game.type}</td>
+          </tr>
+          );
+        })}
+      </tbody>
+    </table>;
 
-  useEffect(() => {
-    gameCtx.list && setGameList(gameCtx.list);
-  }, [gameCtx]);
+  const [gameTable, setGameTable] = useState(allGames);
+
 
   async function onDelete(gameName) {
     if (window.confirm(`Are you sure you wish to delete ${gameName}?`)) {
@@ -22,37 +45,51 @@ export default function DeleteForm(props) {
         console.log(dataFromServer);
         console.log(`The Game ${gameName} Deleted`);
         setMessageOnDelete(`Game ${gameName} is successfully deleted!`);
+        console.log(gameList, "befor");
+        gameList.forEach(element => {
+          if (element.name === gameName) { gameList.pop(element); }
+        });
+        console.log(gameList, "after");
+        const newTable =
+          <table>
+            <thead>
+              <tr>
+                <th>Check to delete</th>
+                <th>Game name</th>
+                <th>Category</th>
+                <th>Game type</th>
+              </tr>
+            </thead>
+            <tbody>
+              {gameList && gameList.map(game => {
+                return (<tr key={game.name}>
+                  <td>
+                    <button onClick={() => onDelete(game.name)}>X</button>
+                  </td>
+                  <td>{game.name} </td>
+                  <td>{game.category}</td>
+                  <td>{game.type}</td>
+                </tr>
+                );
+              })}
+            </tbody>
+          </table>;
+        setGameTable(newTable);
       } catch (error) {
         console.error("error", error);
       };
     }
   }
+
+  useEffect(() => {
+    gameCtx.list && setGameList(gameCtx.list);
+  }, [gameCtx]);
+
   return (
     <div>
       {messageOnDelete && messageOnDelete}
-      <table>
-        <thead>
-          <tr>
-            <th>Check to delete</th>
-            <th>Game name</th>
-            <th>Category</th>
-            <th>Game type</th>
-          </tr>
-        </thead>
-        <tbody>
-          {gameList && gameList.map(game => {
-            return (<tr key={game.name}>
-              <td>
-                <button onClick={() => onDelete(game.name)}>X</button>
-              </td>
-              <td>{game.name} </td>
-              <td>{game.category}</td>
-              <td>{game.type}</td>
-            </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      {gameTable && gameTable}
+
     </div>
   );
 }
