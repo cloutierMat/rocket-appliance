9c4c421853f1f179b13cdf1ee154ba0c6e5a4550
Nikita George <nikkigeorge2@gmail.com>
2021-06-01 11:30:12 -0600

    RA-53 #comment added styling to trivia form

--- frontend/src/pages/contribute/components/DeleteForm.js
+++ frontend/src/pages/contribute/components/DeleteForm.js
@@ -1,54 +1,56 @@
-import { useAuth0 } from '@auth0/auth0-react';
-import React, { useState } from 'react';
+import { useAuth0 } from "@auth0/auth0-react";
+import React, { useState } from "react";
+import styles from "../../../app.module.css";
 
 export default function DeleteForm(props) {
-	const { gameList } = props;
-	const [messageOnDelete, setMessageOnDelete] = useState(null);
-	const { user } = useAuth0();
+  const { gameList } = props;
+  const [messageOnDelete, setMessageOnDelete] = useState(null);
+  const { user } = useAuth0();
 
-	async function onDelete(gameName) {
-		if (window.confirm(`Are you sure you wish to delete ${gameName}?`)) {
-			try {
-				const res = await fetch(`/game/trivia/${gameName}/${user.sub}`, {
-					method: "DELETE",
+  async function onDelete(gameName) {
+    if (window.confirm(`Are you sure you wish to delete ${gameName}?`)) {
+      try {
+        const res = await fetch(`/game/trivia/${gameName}/${user.sub}`, {
+          method: "DELETE",
+        });
+        const dataFromServer = await res.json();
+        console.log(dataFromServer);
+        console.log(`The Game ${gameName} Deleted`);
+        setMessageOnDelete(`Game ${gameName} is successfully deleted!`);
+      } catch (error) {
+        console.error("error", error);
+      }
+    }
+  }
 
-				});
-				const dataFromServer = await res.json();
-				console.log(dataFromServer);
-				console.log(`The Game ${gameName} Deleted`);
-				setMessageOnDelete(`Game ${gameName} is successfully deleted!`);
-			} catch (error) {
-				console.error("error", error);
-			};
-		}
-	}
-
-	return (
-		<div>
-			{messageOnDelete && messageOnDelete}
-			<table>
-				<thead>
-					<tr>
-						<th>Check to delete</th>
-						<th>Game name</th>
-						<th>Category</th>
-						<th>Game type</th>
-					</tr>
-				</thead>
-				<tbody>
-					{gameList && gameList.map(game => {
-						return (<tr key={game.name}>
-							<td>
-								<button onClick={() => onDelete(game.name)}>X</button>
-							</td>
-							<td>{game.name} </td>
-							<td>{game.category}</td>
-							<td>{game.type}</td>
-						</tr>
-						);
-					})}
-				</tbody>
-			</table>
-		</div>
-	);
+  return (
+    <div>
+      {messageOnDelete && messageOnDelete}
+      <table>
+        <thead className={styles["game-title"]}>
+          <tr>
+            <th>Check to delete</th>
+            <th>Game name</th>
+            <th>Category</th>
+            <th>Game type</th>
+          </tr>
+        </thead>
+        <tbody>
+          {gameList &&
+            gameList.map((game) => {
+              return (
+                <tr className={styles["edit"]} key={game.name}>
+                  <td>
+                    <button onClick={() => onDelete(game.name)}>X</button>
+                  </td>
+                  <td>{game.name} </td>
+                  <td>{game.category}</td>
+                  <td>{game.type}</td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
