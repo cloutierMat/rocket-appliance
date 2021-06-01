f0cf1d79c674fb265f6f0e2fcc9794081c887eec
azdelta <azdelta@users.noreply.github.com>
2021-06-01 13:04:09 -0600

    RA-44 #comment 1376th svg update

--- frontend/src/components/NavTitle.js
+++ frontend/src/components/NavTitle.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import navBanner from './images/RAP.svg';
+import navBanner from './images/RA.svg';
 import { Animated } from "react-animated-css";
 import styles from '../app.module.css';
 
