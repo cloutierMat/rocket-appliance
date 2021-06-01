751db680eca8438ea11e2509e8c5ff85044118ba
Will Smit <wsmiteh@gmail.com>
2021-06-01 15:38:55 -0600

    typo fix

--- frontend/src/pages/mainPage/components/About.js
+++ frontend/src/pages/mainPage/components/About.js
@@ -7,7 +7,7 @@ export default function About() {
 
 		<div className={`${styles["text-default"]} ${styles["full-width"]} ${styles["about"]}`}>
 			{/* <Animated animationIn="slideInRight" animationOut="fadeOut" isVisible={true}> */}
-			rocket appliance is a contribution based educatonal game platform designed to expose people to
+			rocket appliance is a contribution based educational game platform designed to expose people to
 			traditionally 'ominous' knowledge in a palatable, gamified format.
 			{/* </Animated> */}
 		</div>
