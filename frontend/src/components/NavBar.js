e4d824ef34c39bcffab6ac875d4765faef7ce3f1
Nikita George <nikkigeorge2@gmail.com>
2021-06-02 14:41:36 -0600

    RA-61 #comment added padding and new logout button

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,30 +1,38 @@
-import React from 'react';
-import NavButton from './NavButton';
-import NavTitle from './NavTitle';
+import React from "react";
+import NavButton from "./NavButton";
+import NavTitle from "./NavTitle";
 import { Animated } from "react-animated-css";
-import styles from '../app.module.css';
-import { useAuth0 } from '@auth0/auth0-react';
+import styles from "../app.module.css";
+import { useAuth0 } from "@auth0/auth0-react";
 import { RiLogoutBoxRFill } from "react-icons/ri";
 
 export default function NavBar() {
-	const { user, logout, isAuthenticated } = useAuth0();
-	return (
-		<nav className={`${styles["flex-container"]} ${styles["full-width"]}`}>
-			<NavTitle />
-			<div className={`${styles["nav-buttons-wrapper"]} ${styles["right-side"]}`}>
-				<Animated animationIn="slideInRight" animationOut="fadeOut" isVisible={true}>
-					{['Learn', 'Contribute'].map(element => {
-						return <NavButton key={element} name={element} />;
-					})}
-				</Animated>
-			</div>
-			{isAuthenticated &&
-				<button className={`${styles["logout-button"]}`} onClick={() => logout()}>
-					<RiLogoutBoxRFill />
-					logout
-				</button>
-			}
-			<button onClick={() => console.log(user)}>log user</button>
-		</nav>
-	);
+  const { user, logout, isAuthenticated } = useAuth0();
+  return (
+    <nav className={`${styles["flex-container"]} ${styles["full-width"]}`}>
+      <NavTitle />
+      <div
+        className={`${styles["nav-buttons-wrapper"]} ${styles["right-side"]}`}
+      >
+        <Animated
+          animationIn="slideInRight"
+          animationOut="fadeOut"
+          isVisible={true}
+        >
+          {["Learn", "Contribute"].map((element) => {
+            return <NavButton key={element} name={element} />;
+          })}
+        </Animated>
+      </div>
+      {isAuthenticated && (
+        <button
+          className={`${styles["logout-button"]}`}
+          onClick={() => logout()}
+        >
+          <RiLogoutBoxRFill />
+        </button>
+      )}
+      {/* <button onClick={() => console.log(user)}>log user</button> */}
+    </nav>
+  );
 }
