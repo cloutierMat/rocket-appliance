f30e0142b13ed6324958989078d9ff725163b1ab
Nikita George <nikkigeorge2@gmail.com>
2021-06-01 19:20:57 -0600

    RA-53 #comment added borders, colors, and spacing in second div

--- frontend/src/pages/contribute/components/ContributeForm.js
+++ frontend/src/pages/contribute/components/ContributeForm.js
@@ -1,45 +1,62 @@
-import React, { useState } from 'react';
-import styles from '../../../app.module.css';
+import React, { useState } from "react";
+import styles from "../../../app.module.css";
 import { AiFillEdit } from "react-icons/ai";
 
 export default function ContributeForm(props) {
-	const { setFormPointer, setToggleFormPointer, messageOnSubmit } = props;
+  const { setFormPointer, setToggleFormPointer, messageOnSubmit } = props;
 
-	const [optionState, setOptionState] = useState('Select a game type');
+  const [optionState, setOptionState] = useState("Select a game type");
 
-	function handleGameType(event) {
-		if (event.target.value === 'Select a game type') {
-			setFormPointer('Select a game type');
-		}
-		else if (event.target.value === 'Trivia') {
-			setFormPointer('Trivia');
-		}
-		setOptionState('Select a game type');
-	}
+  function handleGameType(event) {
+    if (event.target.value === "Select a game type") {
+      setFormPointer("Select a game type");
+    } else if (event.target.value === "Trivia") {
+      setFormPointer("Trivia");
+    }
+    setOptionState("Select a game type");
+  }
 
-	function handleToggle() {
-		setToggleFormPointer(true);
-	}
+  function handleToggle() {
+    setToggleFormPointer(true);
+  }
 
-	return (
-		<div className={`${styles["contribute-form"]}`}>
-			<select value={optionState} onChange={handleGameType} className={`${styles["text-default"]} ${styles["drop-down"]}`}>
-				<option>Select a game type</option>
-				<option>Trivia</option>
-			</select>
-			<hr />
-			<div className={`${styles["edit-wrapper"]}`}>
-				<h3 className={`${styles["text-default"]}`}>Edit or Delete an exisiting game!</h3>
-				<button onClick={() => { setFormPointer('Edit'); handleToggle(); }}>
-					Edit
-						<AiFillEdit />
-				</button>
-				<button onClick={() => { setFormPointer('delete'); handleToggle(); }}>
-					Delete
-					<AiFillEdit />
-				</button>
-			</div>
-			<h2 className="message-on-create_contribute">{messageOnSubmit}</h2>
-		</div>
-	);
-};
+  return (
+    <div className={`${styles["contribute-form"]}`}>
+      <select
+        value={optionState}
+        onChange={handleGameType}
+        className={`${styles["text-default"]} ${styles["drop-down"]}`}
+      >
+        <option>Select a game type</option>
+        <option>Trivia</option>
+      </select>
+      <hr />
+      <div className={`${styles["edit-wrapper"]}`}>
+        <h3 className={`${styles["text-default"]}`}>
+          Edit or Delete an exisiting game!
+        </h3>
+        <button
+          onClick={() => {
+            setFormPointer("Edit");
+            handleToggle();
+          }}
+        >
+          Edit
+          <AiFillEdit />
+        </button>
+        <button
+          onClick={() => {
+            setFormPointer("delete");
+            handleToggle();
+          }}
+        >
+          Delete
+          <AiFillEdit />
+        </button>
+      </div>
+      <h2 className={styles["message-on-create_contribute"]}>
+        {messageOnSubmit}
+      </h2>
+    </div>
+  );
+}
