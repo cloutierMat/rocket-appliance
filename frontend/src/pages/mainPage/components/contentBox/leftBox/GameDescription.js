b55d4058c7ed67c29539f59b6e4a1f680871daea
azdelta <zohoorparvaz@gmail.com>
2021-06-02 00:14:06 -0600

    RA-51 #done #comment description disappears as we have no result in gallery and comes back as we clear the search bar

--- frontend/src/pages/mainPage/components/contentBox/leftBox/GameDescription.js
+++ frontend/src/pages/mainPage/components/contentBox/leftBox/GameDescription.js
@@ -7,29 +7,33 @@ export default function GameDescription(props) {
 	const { game } = props;
 
 	const [gameToDisplay, setGameToDisplay] = useState();
-
 	const gameCtx = useContext(GameContext);
 
 	useEffect(() => {
 		setGameToDisplay(gameCtx.list.find(elem => elem.name === game));
 	}, [game, gameCtx.list]);
 
+	const descriptionContent =
+		gameToDisplay &&
+		<Animated animationIn="slideInLeft" animationOut="fadeOut" isVisible={true}>
+			<div className={`${styles["game-description"]} ${styles["text-default"]}`}>
+				<h3>{gameToDisplay.name}</h3>
+				<p>Created by {gameToDisplay.author}</p>
+				<hr></hr>
+				<div>
+					<h4>Learn about {gameToDisplay.category} by playing {gameToDisplay.name} {gameToDisplay.type}<br /> {gameToDisplay.description}</h4>
+				</div>
+			</div>
+		</Animated>;
+
+
+	if (!gameCtx.filteredByFragment.length) {
+
+	}
+
 	return (
 		<div>
-
-			{gameToDisplay &&
-				<Animated animationIn="slideInLeft" animationOut="fadeOut" isVisible={true}>
-					<div className={`${styles["game-description"]} ${styles["text-default"]}`}>
-						<h3>{gameToDisplay.name}</h3>
-						<p>Created by {gameToDisplay.author}</p>
-						<hr></hr>
-						<div>
-							<h4>Learn about {gameToDisplay.category} by playing {gameToDisplay.name} {gameToDisplay.type}<br /> {gameToDisplay.description}</h4>
-						</div>
-					</div>
-				</Animated>
-			}
-
+			{ gameCtx.filteredByFragment.length && descriptionContent}
 		</div>
 	);
 };
