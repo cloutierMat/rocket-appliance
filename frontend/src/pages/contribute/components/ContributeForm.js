0f70184bfcafb1a95da088d84c6c97fc28cfe381
azdelta <zohoorparvaz@gmail.com>
2021-06-02 09:28:16 -0600

    RA-56 #comment change the way message disappears from useContext to useEffect

--- frontend/src/pages/contribute/components/ContributeForm.js
+++ frontend/src/pages/contribute/components/ContributeForm.js
@@ -1,5 +1,4 @@
 import React, { useState, useContext } from "react";
-import SubmitContext from '../../../context/SubmitContext';
 import styles from "../../../app.module.css";
 import { AiFillEdit } from "react-icons/ai";
 
@@ -8,7 +7,6 @@ export default function ContributeForm(props) {
 
   const [optionState, setOptionState] = useState("Select a game type");
 
-  const submitCtx = useContext(SubmitContext);
   function handleGameType(event) {
     if (event.target.value === "Select a game type") {
       setFormPointer("Select a game type");
@@ -39,7 +37,6 @@ export default function ContributeForm(props) {
         </h3>
         <button
           onClick={() => {
-            submitCtx.setMessage(() => "");
             setFormPointer("Edit");
             handleToggle();
           }}
@@ -49,7 +46,6 @@ export default function ContributeForm(props) {
         </button>
         <button
           onClick={() => {
-            submitCtx.setMessage(() => "");
             setFormPointer("delete");
             handleToggle();
           }}
--- frontend/src/pages/mainPage/components/contentBox/rightBox/SuggestionBox.js
+++ frontend/src/pages/mainPage/components/contentBox/rightBox/SuggestionBox.js
@@ -24,5 +24,5 @@ export default function SuggestionsBox() {
 					<GameDescription game={gameToDisplay} />
 				</section>
 			}
-		</div >);
+		</div>);
 }
